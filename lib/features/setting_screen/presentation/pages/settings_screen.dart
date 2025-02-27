e8afc8606789c528bc28d838bf240d313bb4c4d9
amrMohamed-pureSoft <amr.mohamed@pure-soft.com>
2025-02-27 19:34:44 +0200

    edit

--- lib/features/setting_screen/presentation/pages/settings_screen.dart
+++ lib/features/setting_screen/presentation/pages/settings_screen.dart
@@ -1,9 +1,7 @@
-import 'package:career/core/themes/styles/app_text_style.dart';
 import 'package:career/core/widgets/screen_wrapper.dart';
 import 'package:career/features/setting_screen/presentation/views/setting_page_app_bar.dart';
 import 'package:career/features/setting_screen/presentation/views/setting_page_body.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
