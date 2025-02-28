636d148b6481ba42bcd256a9d947f3ee4c59913c
amrMohamed-pureSoft <amr.mohamed@pure-soft.com>
2025-02-28 02:31:09 +0200

    edit

--- lib/features/register_as_business/presentation/page/register_as_business.dart
+++ lib/features/register_as_business/presentation/page/register_as_business.dart
@@ -1,14 +1,8 @@
 import 'dart:ui';
-import 'package:career/core/app_texts/app_localizations.dart';
-import 'package:career/core/widgets/app_contry_code_picker.dart';
-import 'package:career/core/widgets/app_drop_down.dart';
-import 'package:career/core/widgets/app_text_field.dart';
-import 'package:career/core/widgets/forward_widget.dart';
-import 'package:career/core/widgets/primary_container.dart';
 import 'package:career/core/widgets/screen_wrapper.dart';
-import 'package:career/core/widgets/title_widget.dart';
 import 'package:career/features/register_as_business/presentation/manager/business_cubit.dart';
 import 'package:career/features/register_as_business/presentation/manager/business_state.dart';
+import 'package:career/features/register_as_business/presentation/view/business_core_information.dart';
 import 'package:career/features/register_as_business/presentation/view/company_image.dart';
 import 'package:career/features/register_as_business/presentation/view/company_info.dart';
 import 'package:career/features/register_as_business/presentation/view/company_language.dart';
@@ -16,7 +10,6 @@ import 'package:career/gen/assets.gen.dart';
 import 'package:expandable_page_view/expandable_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 
 class RegisterAsBusiness extends StatelessWidget {
@@ -77,67 +70,6 @@ class RegisterAsBusiness extends StatelessWidget {
             );
           },
         ),
-      ),
-    );
-  }
-}
-
-class BusinessCoreInformation extends StatelessWidget {
-  const BusinessCoreInformation({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return PrimaryContainer(
-      child: Column(
-        spacing: 16.h,
-        children: [
-          TitleWidget(
-            title: AppLocalizations.of(context).coreInfo,
-            onTap: () => context.read<RegisterAsBusinessCubit>().previous(),
-          ),
-          const SizedBox(),
-          AppTextField(
-            hint: AppLocalizations.of(context).fullName,
-            prefixIcon: const Icon(
-              Icons.home_work_outlined,
-            ),
-          ),
-          AppTextField(
-            hint: AppLocalizations.of(context).email,
-            prefixIcon: const Icon(
-              Icons.email,
-            ),
-          ),
-          AppTextField(
-            hint: AppLocalizations.of(context).phone,
-            prefixIcon: const CountryCodePrefixIcon(),
-          ),
-          AppTextField(
-            hint: AppLocalizations.of(context).password,
-            prefixIcon: const Icon(
-              Icons.lock,
-            ),
-            scure: true,
-          ),
-          AppTextField(
-            hint: AppLocalizations.of(context).dateOfCreate,
-            prefixIcon: const Icon(
-              Icons.date_range_outlined,
-            ),
-          ),
-          AppDropdown(
-            prefixIcon: const Icon(
-              Icons.flag,
-            ),
-            items: [],
-            onChanged: (value) {},
-            hint: AppLocalizations.of(context).nationality,
-          ),
-          const SizedBox(),
-          ForwardWidget(
-            onPressed: () => context.read<RegisterAsBusinessCubit>().next(),
-          ),
-        ],
       ),
     );
   }
---
+++ lib/features/register_as_business/presentation/view/business_core_information.dart
@@ -0,0 +1,72 @@
+import 'package:career/core/app_texts/app_localizations.dart';
+import 'package:career/core/widgets/app_contry_code_picker.dart';
+import 'package:career/core/widgets/app_drop_down.dart';
+import 'package:career/core/widgets/app_text_field.dart';
+import 'package:career/core/widgets/forward_widget.dart';
+import 'package:career/core/widgets/primary_container.dart';
+import 'package:career/core/widgets/title_widget.dart';
+import 'package:career/features/register_as_business/presentation/manager/business_cubit.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class BusinessCoreInformation extends StatelessWidget {
+  const BusinessCoreInformation({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PrimaryContainer(
+      child: Column(
+        spacing: 16.h,
+        children: [
+          TitleWidget(
+            title: AppLocalizations.of(context).coreInfo,
+            onTap: () => context.read<RegisterAsBusinessCubit>().previous(),
+          ),
+          const SizedBox(),
+          AppTextField(
+            hint: AppLocalizations.of(context).fullName,
+            prefixIcon: const Icon(
+              Icons.home_work_outlined,
+            ),
+          ),
+          AppTextField(
+            hint: AppLocalizations.of(context).email,
+            prefixIcon: const Icon(
+              Icons.email,
+            ),
+          ),
+          AppTextField(
+            hint: AppLocalizations.of(context).phone,
+            prefixIcon: const CountryCodePrefixIcon(),
+          ),
+          AppTextField(
+            hint: AppLocalizations.of(context).password,
+            prefixIcon: const Icon(
+              Icons.lock,
+            ),
+            scure: true,
+          ),
+          AppTextField(
+            hint: AppLocalizations.of(context).dateOfCreate,
+            prefixIcon: const Icon(
+              Icons.date_range_outlined,
+            ),
+          ),
+          AppDropdown(
+            prefixIcon: const Icon(
+              Icons.flag,
+            ),
+            items: [],
+            onChanged: (value) {},
+            hint: AppLocalizations.of(context).nationality,
+          ),
+          const SizedBox(),
+          ForwardWidget(
+            onPressed: () => context.read<RegisterAsBusinessCubit>().next(),
+          ),
+        ],
+      ),
+    );
+  }
+}
